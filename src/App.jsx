6fe672b6728b1f5f39868d2c70f11d48fc950e89
unknown <danielfrancis442@gmail.com>
2024-10-09 16:21:43 +0100

    Uploading all the work

--- src/App.jsx
+++ src/App.jsx
@@ -121,27 +121,56 @@
 
 // Updating the Screen 
 
+// import { useState } from 'react';
+
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Counters that update separately</h1>
+//       <MyButton />
+//       <MyButton />
+//     </div>
+//   );
+// }
+
+// function MyButton() {
+//   const [count, setCount] = useState(0);
+
+//   function handleClick() {
+//     setCount(count + 1);
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       Clicked {count} times
+//     </button>
+//   );
+// }
+
+
+//Sharing Data Between Components
+
 import { useState } from 'react';
 
 export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
-      <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
 
-function MyButton() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 3);
-  }
-
+function MyButton({ count, onClick }) {
   return (
-    <button onClick={handleClick}>
+    <button onClick={onClick}>
       Clicked {count} times
     </button>
   );
