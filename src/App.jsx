52acb5800829a596a8448f6e887b238f00b10264
unknown <danielfrancis442@gmail.com>
2025-04-16 20:35:45 +0100

    Updating...

--- src/App.jsx
+++ src/App.jsx
@@ -255,4 +255,19 @@ function FilterableProductTable({ products }) {
       <h2>Keep Reading</h2>
       <SearchBar />
       <ProductTable products={products} />
-    </div
+    </div>
+  );
+}
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+export default function App() {
+  return <FilterableProductTable products={PRODUCTS} />;
+}
